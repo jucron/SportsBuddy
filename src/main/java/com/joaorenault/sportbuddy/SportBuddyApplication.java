6af1a722004dda6f56a8966f205de1d19cc1c841
jucron <jucron@gmail.com>
2021-08-31 14:58:24 +0100

    Starting project.

---
+++ src/main/java/com/joaorenault/sportbuddy/SportBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.joaorenault.sportbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportBuddyApplication.class, args);
+    }
+
+}
